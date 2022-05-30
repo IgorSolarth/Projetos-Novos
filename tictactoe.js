5708744dea82d29aa79b1ac5c8ca93d862cd2dcd
Igor Solarth <106268722+IgorSolarth@users.noreply.github.com>
2022-05-30 12:12:37 -0400

    up

--- tictactoe.js
+++ tictactoe.js
@@ -1,25 +1,6 @@
 const Tictactoe = () => {
-    const texts = [
-        "Aperte o play para jogar",
-        "joga",
-        "venceu o jogo",
-    ];
-    const self = {
-        text: texts[1]
-    };
-    const template = `
-        <div>
-            <h1>Jogo da velha</h1>
-            <p>
-                Jogar contra o computador
-            </p>
-        </div>
-    
-    
-    
-    
-    `;
-
+    const self = {};
+    const template = `<p>Teste</p>`;
     
     return lemonade.element(template, self);
-}    
+}
